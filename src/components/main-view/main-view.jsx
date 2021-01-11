36ab530f6e745949e663b253f7e149a7a16885f4
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-01-10 22:29:27 -0500

    Add MovieView.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-	// One of the "hooks" available in a React Component
+	constructor() {
+		super();
+
+		this.state = {
+			movies: null,
+			selectedMovie: null,
+		};
+	}
 	componentDidMount() {
 		axios
 			.get('https://movie-api-on-heroku.herokuapp.com/movies')
@@ -18,18 +26,27 @@ export class MainView extends React.Component {
 				console.log(error);
 			});
 	}
+	onMovieClick(movie) {
+		this.setState({
+			selectedMovie: movie,
+		});
+	}
 
 	render() {
-		const { movies } = this.state;
+		const { movies, selectedMovie } = this.state;
 
 		// Before the movies have been loaded
 		if (!movies) return <div className="main-view" />;
 
 		return (
 			<div className="main-view">
-				{movies.map((movie) => (
-					<MovieCard key={movie._id} movie={movie} />
-				))}
+				{selectedMovie ? (
+					<MovieView movie={selectedMovie} />
+				) : (
+					movies.map((movie) => (
+						<MovieCard key={movie._id} movie={movie} onClick={(movie) => this.onMovieClick(movie)} />
+					))
+				)}
 			</div>
 		);
 	}
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,12 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class movieView extends Component {
+export class MovieView extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {};
+	}
+
 	render() {
+		const { movie } = this.props;
+
+		if (!movie) return null;
+
 		return (
-			<div>
-				<img src="{this.state.ImagePath}" alt="movie poster"></img>
-				<h3>Title: {this.state.title}</h3>
-				<h3>Description: {this.state.description}</h3>
+			<div className="movie-view">
+				<img className="movie-poster" src={movie.ImagePath} />
+				<div className="movie-title">
+					<span className="label">Title: </span>
+					<span className="value">{movie.Title}</span>
+				</div>
+				<div className="movie-description">
+					<span className="label">Description: </span>
+					<span className="value">{movie.Description}</span>
+				</div>
+
+				<div className="movie-genre">
+					<span className="label">Genre: </span>
+					<span className="value">{movie.Genre.Name}</span>
+				</div>
+				<div className="movie-director">
+					<span className="label">Director: </span>
+					<span className="value">{movie.Director.Name}</span>
+				</div>
 			</div>
 		);
 	}
