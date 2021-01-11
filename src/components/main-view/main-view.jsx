c55c1c242011637ce75aa3ea26fd45fee3363cf2
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-01-10 21:54:15 -0500

    Add view components.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import axios from axios;
+import axios from 'axios';
+
+import { MovieCard } from '../movie-card/movie-card';
 
 export class MainView extends React.Component {
-  componentDidMount(){
-    axios.get('https://movie-api-on-heroku.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        })
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
-  }
+	// One of the "hooks" available in a React Component
+	componentDidMount() {
+		axios
+			.get('https://movie-api-on-heroku.herokuapp.com/movies')
+			.then((response) => {
+				// Assign the result to the state
+				this.setState({
+					movies: response.data,
+				});
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
 
 	render() {
-    const {movies} = this.state;
-    if (!movies) return <div className ="main-view" />;
+		const { movies } = this.state;
 
-    return(
-      <div className = 'main-view'>
-        {movies.map(movie=>(
-          <div className = 'movie-card' key={movie._id}>{movie.Title}</div>
-        ))}
-      </div>
-    )
+		// Before the movies have been loaded
+		if (!movies) return <div className="main-view" />;
+
+		return (
+			<div className="main-view">
+				{movies.map((movie) => (
+					<MovieCard key={movie._id} movie={movie} />
+				))}
+			</div>
+		);
 	}
 }
