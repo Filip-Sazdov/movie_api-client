28fefab5303e5afc19ca3dcd38c7f791f7a81961
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-01-16 00:15:16 -0500

    Fix form in view.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
 	const [username, setUsername] = useState('');
@@ -13,18 +15,30 @@ export function LoginView(props) {
 	};
 
 	return (
-		<form>
-			<label>
-				Username:
-				<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-			</label>
-			<label>
-				Password:
-				<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-			</label>
-			<button type="button" onClick={handleSubmit}>
+		<Form>
+			<h3>Please Login</h3>
+			<Form.Group>
+				<Form.Label>Username</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="Enter username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+			</Form.Group>
+
+			<Form.Group>
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					type="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+			</Form.Group>
+			<Button variant="primary" type="button" onClick={handleSubmit}>
 				Submit
-			</button>
-		</form>
+			</Button>
+		</Form>
 	);
 }
