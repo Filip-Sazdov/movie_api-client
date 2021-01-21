785112138307c9c871a52f0ac05feff6dd05db31
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-01-21 15:33:52 -0500

    Add link to moviedId endpoint.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from 'react-router-dom';
+
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
 	render() {
-		// This is given to the <MovieCard/> component by the outer world
-		// which, in this case, is `MainView`, as `MainView` is what’s
-		// connected to your database via the movies endpoint of your API
-		const { movie, onClick } = this.props;
+		const { movie } = this.props;
 
 		return (
 			<Card style={{ width: '16rem' }}>
@@ -18,9 +17,9 @@ export class MovieCard extends React.Component {
 				<Card.Body>
 					<Card.Title>{movie.Title}</Card.Title>
 					<Card.Text>{movie.Description}</Card.Text>
-					<Button onClick={() => onClick(movie)} variant="link">
-						Open
-					</Button>
+					<Link to={`/movies/${movie._id}`}>
+						<Button variant="link">Open</Button>
+					</Link>
 				</Card.Body>
 			</Card>
 		);
@@ -31,5 +30,5 @@ MovieCard.propTypes = {
 	movie: PropTypes.shape({
 		Title: PropTypes.string,
 	}).isRequired,
-	onClick: PropTypes.func.isRequired,
+	// onClick: PropTypes.func.isRequired,
 };
