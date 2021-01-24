322d3363fcd860326f5fd56ddc90b4b88013f72c
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-01-24 17:05:29 -0500

    Add about info and media.

--- src/components/shared-components/about/about.jsx
+++ src/components/shared-components/about/about.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
 export class About extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>This is an about page</h1>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<Card style={{ width: '400px' }}>
+					<Card.Img variant="top" src={'../../img/portrait_square.jpg'} />
+					<Card.Body>
+						<Card.Title></Card.Title>
+						<Card.Text>
+							Hi. I'm Filip, an aspiring web developer looking to expand my programming skills and knowledge. My goal is
+							to build projects, make some money and have fun while doing it. This is my portfolio webpage. Please feel
+							free to navigate the links and check out my work or contact me.
+						</Card.Text>
+					</Card.Body>
+				</Card>
 			</div>
 		);
 	}
