0f9fd17551dcf68b35416417c235996ade171497
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-01-27 23:15:44 -0500

    Add same genre list of movies.

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -27,11 +27,11 @@ export class GenreView extends React.Component {
 						<img className="genre-poster " src="https://via.placeholder.com/150" />
 						<div className="genre-title ">
 							{/* <span className="label">Name: </span> */}
-							<span className="value">{genre.Name}</span>
+							<span className="value">{genre.Genre.Name}</span>
 						</div>
 						<div className="genre-description ">
 							{/* <span className="label">Description: </span> */}
-							<span className="value">{genre.Description}</span>
+							<span className="value">{genre.Genre.Description}</span>
 						</div>
 						<Link to={`/`}>
 							<Button variant="link">Return</Button>
@@ -39,6 +39,35 @@ export class GenreView extends React.Component {
 					</Col>
 					<Col className="col-3" />
 				</Row>
+				<Container>
+					<h4 className="mt-4">Some {genre.Genre.Name} movies</h4>
+					<div className="d-flex row mt-3 ml-2">
+						{movies.map((movie) => {
+							if (movie.Genre.Name === genre.Genre.Name) {
+								return (
+									<div key={movie._id}>
+										<Card className="mb-3 mr-2 h-100" style={{ width: '16rem' }}>
+											<Card.Img variant="top" src={movie.ImagePath} />
+											<Card.Body>
+												<Link className="text-muted" to={`/movies/${movie._id}`}>
+													<Card.Title>{movie.Title}</Card.Title>
+												</Link>
+												<Card.Text>{movie.Description.substring(0, 90)}...</Card.Text>
+											</Card.Body>
+											<Card.Footer className="bg-white border-top-0">
+												<Link to={`/movies/${movie._id}`}>
+													<Button variant="link" className="read-more-link pl-0">
+														Read more
+													</Button>
+												</Link>
+											</Card.Footer>
+										</Card>
+									</div>
+								);
+							}
+						})}
+					</div>
+				</Container>
 			</Container>
 		);
 	}
